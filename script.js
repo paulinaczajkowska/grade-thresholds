c8c5320917da95eb3bfdbea2eceadd1819224628
paulinaczajkowska <152791126+paulinaczajkowska@users.noreply.github.com>
2025-07-26 22:31:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,16 +10,26 @@ function calculateThresholds() {
         { grade: 5, percent: 90},
         { grade: 4, percent: 75},
         { grade: 3, percent: 50},
-        { grade: 2, percent: 35}
+        { grade: 2, percent: 35},
+        { grade: '2 dostosowanie', percent: 25}
     ];
 
     let resultHTML = `<h2>Próg:</h2><ul>`;
     thresholds.forEach(t => {
         const pointValue = (t.percent / 100) * maxPoints;
-        resultHTML += `<li>Ocena ${t.grade}: od ${Math.ceil(pointValue)} punktów</li>`;
+        resultHTML += `<li>Ocena ${t.grade}: od ${Math.ceil(pointValue)} points</li>`;
 
     });
     resultHTML += `</ul>`;
 
     document.getElementById('result').innerHTML = resultHTML;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.getElementById('maxPoints');
+  input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      calculateThresholds();
+    }
+  });
+});
